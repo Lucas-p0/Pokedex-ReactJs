19209359415a46ebe83bc8037c82db7f50eb8ad3
Lucas Pinheiro <lucaslogos@gmail.com>
2022-10-21 17:24:50 +0000

    Update

--- src/ api.js
+++ src/ api.js
@@ -1,10 +1,9 @@
 export const searchPokemon = async(pokemon)=>{
     try{
-        let url = `https://pokeapi.co/v2/pokemon/${pokemon}`
-        const response = fetch(url)
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+        const response = await fetch(url)
         return await response.json()
-        return
-
+        
     }catch{
         console.log("Error:")
     }
