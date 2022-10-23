4e6e31c3d429d88e67a800d2001c171493512d6e
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2022-10-22 22:45:27 -0300

    UpDate

--- src/componentes/SearchBar/index.js
+++ src/componentes/SearchBar/index.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import './Input.css';
 
-const Searchbar=(props)=>{
-    const {onSearch} = props;
+const Searchbar = (props) => {
+    const { onSearch } = props;
     const [search, setSearch] = useState('dito')
 
     const onChangeHandler = (e) => {
         console.log("pokemon:", e.target.value)
         setSearch(e.target.value)
-        
+
     }
 
-    const onButtonCliclHandler =() => {
+    const onButtonCliclHandler = () => {
 
         onSearch(search)
         console.log("Pokemon:", search)
     }
-    return(
+    return (
         <div className="input-container">
             <input placeholder="Bucar pokemon" onChange={onChangeHandler} />
             <button onClick={onButtonCliclHandler} > Buscar</button>
